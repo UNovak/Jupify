c4bab85ae15300d818c7c66e938a71e9c869ac36
UNovak <88665587+UNovak@users.noreply.github.com>
2024-10-16 19:25:46 +0200

    feat: implement an endpoint to handle cron job result

--- server/index.ts
+++ server/index.ts
@@ -6,7 +6,26 @@ app.get('/', async (c) => {
   return c.text('Hello Hono!')
 })
 
+app.post('/update', async (c) => {
+  const jup: ScraperResponse = await c.req.json()
+  if (jup.status === 'Completed') return c.text('No vote in progress')
+  if (jup.status === 'In progress') {
+    // send email
+    return c.text('Vote ongoing')
+  }
+  return c.json('Unknown Error')
+})
+
 export default {
   fetch: app.fetch,
   port: process.env.PORT || 3000,
+}
+
+type ScraperResponse = {
+  title: string
+  voted: string
+  status: string
+  result: string
+  start: string
+  end: string
 }
