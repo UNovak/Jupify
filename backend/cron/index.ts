9293d33fee14f7592c7e27de808c16a328c63de7
UNovak <88665587+UNovak@users.noreply.github.com>
2024-10-20 21:56:02 +0200

    refactor: remove cron
    Since the machine will be started once per day by fly.io:
    - Remove cronjob dependancy
    - Remove cronjob functionality
    - Make scraper run once at boot

--- backend/cron/index.ts
+++ backend/cron/index.ts
@@ -1,24 +1,14 @@
-import { CronJob } from 'cron'
 import ky from 'ky'
 import { scraper } from './scraper'
 
-const startCron = () => {
-  const cronJob = new CronJob(
-    // Every 8 hours at hh:05
-    '5 */8 * * *',
-    async () => {
-      const result = await scraper() // Run the scraper
-      ky.post(`${process.env.DOMAIN}/update`, {
-        // Send the data to the server
-        json: {
-          ...result,
-        },
-      })
-    }, // onTick
-    null, // onComplete
-    true, // start
-    'utc', // timeZone
-  )
+const runScraper = async () => {
+  const result = await scraper() // Run the scraper
+  ky.post(`${process.env.DOMAIN}/update`, {
+    // Send the data to the server
+    json: {
+      ...result,
+    },
+  })
 }
 
-startCron()
+runScraper()
