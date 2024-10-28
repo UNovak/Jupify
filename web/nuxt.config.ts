540973ca79a6b199f7491f049d09f337d9c0f937
UNovak <88665587+UNovak@users.noreply.github.com>
2024-10-28 23:31:49 +0100

    feat: add custom tailwindCSS config

--- web/nuxt.config.ts
+++ web/nuxt.config.ts
@@ -2,5 +2,31 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   modules: ['@nuxtjs/tailwindcss'],
+  tailwindcss: {
+    viewer: false,
+    config: {
+      theme: {
+        extend: {
+          colors: {
+            'grad-start': '#16094E',
+            'grad-end': '#2A1581',
+            primary: '#25DAC5',
+            secondary: '#482BE7',
+          },
+          fontFamily: {
+            primary: ['Poppins', 'sans-serif'],
+            secondary: ['DM Sans', 'sans-serif'],
+          },
+          lineHeight: {
+            title: '86px',
+            normal: '26px',
+          },
+          fontSize: {
+            normal: '22px',
+          },
+        },
+      },
+    },
+  },
   devtools: { enabled: true },
 })
