7acbdbec9bb9d1f93c00414207f28324f4d2f587
UNovak <88665587+UNovak@users.noreply.github.com>
2024-10-30 17:44:17 +0100

    feat: add page metadata

--- web/app.vue
+++ web/app.vue
@@ -1,3 +1,10 @@
+<script setup lang="ts">
+useHead({
+  title: 'Jupify',
+  meta: [{ name: 'description', content: 'Jupify landing page' }],
+})
+</script>
+
 <template>
   <div>Jupify</div>
 </template>
