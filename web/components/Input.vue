e9dec3dbcda24da8db51ab6180654db54f34d041
UNovak <88665587+UNovak@users.noreply.github.com>
2024-11-03 06:00:40 +0100

    feat: add textarea variant with dynamic styling
    - Introduce `textarea` as a new variant, allowing to render a `<textarea>` element
    - Add conditional rendering to switch between `<input>` and `<textarea>`
    - Apply `cva` for styling

--- web/components/Input.vue
+++ web/components/Input.vue
@@ -1,12 +1,33 @@
 <script setup lang="ts">
+import { cva, type VariantProps } from 'cva'
+
 const emit = defineEmits(['update:modelValue'])
 const props = defineProps({
   type: { type: String, default: 'text' },
   aria: { type: String, default: 'input field' },
   modelValue: { type: [String, Number, null], default: null },
-  variant: { type: String, default: 'text' },
+  variant: {
+    type: String as PropType<'text' | 'textarea'>,
+    default: 'text',
+  },
   placeholder: String,
   isRequired: Boolean,
+})
+
+const inputClass = computed(() => {
+  return cva(
+    'appearance-none border-2 border-white/20 bg-transparent text-xl leading-normal text-white/40 focus:outline-none focus:ring-2 focus:ring-white/40',
+    {
+      variants: {
+        variant: {
+          textarea: 'block w-3/4 rounded-[30px] p-6',
+          text: 'h-[60px] w-[350px] rounded-[100px] py-2 pl-8 align-middle ',
+        },
+      },
+    },
+  )({
+    variant: props.variant,
+  })
 })
 
 const updateValue = (e: Event) => {
@@ -16,12 +37,25 @@ const updateValue = (e: Event) => {
 </script>
 
 <template>
-  <input
-    :type="props.type"
-    class="h-[60px] w-[350px] appearance-none rounded-[100px] border-2 border-white/20 bg-transparent py-2 pl-8 align-middle text-xl leading-normal text-white/40 focus:outline-none focus:ring-2 focus:ring-white/40"
-    :placeholder="props.placeholder"
-    :value="props.modelValue"
-    @input="updateValue"
-    :required="props.isRequired"
-    :aria-label="props.aria" />
+  <template v-if="props.variant === 'textarea'">
+    <textarea
+      rows="6"
+      :class="inputClass"
+      :placeholder="props.placeholder"
+      :value="props.modelValue"
+      @input="updateValue"
+      :required="props.isRequired"
+      :aria-label="props.aria" />
+  </template>
+
+  <template v-else>
+    <input
+      :type="props.type"
+      :class="inputClass"
+      :placeholder="props.placeholder"
+      :value="props.modelValue"
+      @input="updateValue"
+      :required="props.isRequired"
+      :aria-label="props.aria" />
+  </template>
 </template>
