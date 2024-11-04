34481bd98546ea973b63251b239dec3a1c2af5e0
UNovak <88665587+UNovak@users.noreply.github.com>
2024-11-04 01:15:13 +0100

    fix: adjust spacing after adding forms

--- web/components/Section.vue
+++ web/components/Section.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="flex min-h-screen flex-col items-center justify-center">
-    <h1
-      class="font-secondary leading-title whitespace-pre pb-8 text-center text-7xl font-bold text-white">
-      <slot name="title" />
-    </h1>
-    <p
-      class="font-secondary text-normal whitespace-pre pb-20 text-center leading-8 text-white">
-      <slot name="description" />
-    </p>
+  <div
+    class="flex min-h-screen w-full flex-col items-center justify-center gap-24">
+    <div class="flex flex-col gap-10">
+      <h1
+        class="font-secondary leading-title whitespace-pre text-center text-7xl font-bold text-white">
+        <slot name="title" />
+      </h1>
+      <p
+        class="font-secondary text-normal whitespace-pre text-center leading-8 text-white">
+        <slot name="description" />
+      </p>
+    </div>
     <slot />
   </div>
 </template>
