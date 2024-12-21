f4497c4b903df7ece7ec9c131ac419a563728d0a
UNovak <88665587+UNovak@users.noreply.github.com>
2024-12-21 15:22:47 +0100

    feat: implement subscribers endpoint (unfinished)
    - Implements adding a new entry to subscribers table
    - Only implements the database function
    - Still needs implementing sending verification email

--- backend/server/index.ts
+++ backend/server/index.ts
@@ -1,5 +1,7 @@
 import { Hono } from 'hono'
 import { sendMail } from './sendMail'
+import { generateToken } from './utils/token'
+import { subscribe } from './utils/turso'
 
 const app = new Hono()
 
@@ -13,6 +15,36 @@ app.post('/update', async (c) => {
   if (jup.status === 'Completed') return c.text('No vote in progress')
   const res = await sendMail(process.env.MAIL as string)
   return c.json(res)
+})
+
+// Add email to subscribers database, send verification email
+app.post('/subscribers', async (c) => {
+  const { email } = await c.req.json()
+  const token = await generateToken(email, 30) // create a JWT valid for 30 minutes
+  console.log(token)
+  const res = await subscribe(email, token) // insert into db
+
+  // if insert !ok
+  if (!res) {
+    return c.json({
+      status: 500,
+      message: 'Something went wrong when inserting',
+    })
+  }
+
+  // if insert sucessfull
+
+  /*
+
+    implement sending a verification email here
+
+    ) */
+
+  // server response
+  return c.json({
+    status: 201,
+    message: `Inserted ${email}`,
+  })
 })
 
 export default {
