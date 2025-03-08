0394455a1b7a65a13732bfa04c2f6243a44561e7
UNovak <88665587+UNovak@users.noreply.github.com>
2025-03-08 18:36:06 +0100

    fix: make modal client only

--- web/components/Modal.vue
+++ web/components/Modal.vue
@@ -3,12 +3,15 @@ const { isModalOpen, hideModal, component } = useModal()
 </script>
 
 <template>
-  <Teleport to="#modal">
-    <div
-      class="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50"
-      v-if="isModalOpen"
-      @click="hideModal()">
-      <component :is="component" @close="hideModal()" class="z-50" />
-    </div>
-  </Teleport>
+  <ClientOnly>
+    <Teleport to="#modal">
+      <div
+        tabindex="-1"
+        class="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50"
+        v-if="isModalOpen"
+        @click="hideModal()">
+        <component :is="component" @close="hideModal()" class="z-50" />
+      </div>
+    </Teleport>
+  </ClientOnly>
 </template>
