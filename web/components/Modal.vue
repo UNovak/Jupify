a9af7d869e663bdc252a4c622c0e49a129360322
UNovak <88665587+UNovak@users.noreply.github.com>
2025-03-10 10:03:22 +0100

    fix: add passing props to <component>

--- web/components/Modal.vue
+++ web/components/Modal.vue
@@ -10,7 +10,11 @@ const { isModalOpen, hideModal, component } = useModal()
         class="fixed inset-0 z-10 flex items-center justify-center bg-black bg-opacity-50"
         v-if="isModalOpen"
         @click="hideModal()">
-        <component :is="component" @close="hideModal()" class="z-50" />
+        <component
+          :is="component"
+          @close="hideModal()"
+          class="z-50"
+          v-bind="component?.props || {}" />
       </div>
     </Teleport>
   </ClientOnly>
